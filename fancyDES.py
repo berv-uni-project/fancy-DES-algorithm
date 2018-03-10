cb7d7299ecd82429091e34bc20f4ecbbf74d29b3
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2018-03-10 11:13:00 +0700

    message to blocks

--- fancyDES.py
+++ fancyDES.py
@@ -53,17 +53,49 @@ class FancyDES():
             output.append(list(item))
         return output
 
+    def messageToBlock(self, message = None):
+        output = [['0x{:02X}'.format(ord(message[i+j])) for i in range(4)] for j in range(4)]
+        return output
+
+    def messageToBlocks(self, n = 0, message = None):
+        position = 0
+        blocks = []
+        for i in range(n):
+            block = message[position:position+16]
+            blocks.append(self.messageToBlock(block))
+            position += 16
+        return blocks
+
+    def getBlocks(self):
+        # expand to 16 byte each
+        temp = self.message
+        while len(temp) % 16 != 0:
+            temp += '.'
+        print(temp)
+        # if sum of block odd, add one block
+        sum_blocks = len(temp) // 16
+        if sum_blocks % 2 == 1:
+            sum_blocks += 1
+            temp += '................'
+        blocks = self.messageToBlocks(sum_blocks, temp)
+        return blocks
+
     def generate_chiper(self):
         sum = 0
         for i in self.key:
             sum += ord(i)
-        print(sum)
         random.seed(sum)
         round = random.randint(1,25)
-        print(round)
         if round < 7:
             round = 7
+        self.position = 0
+        blocks = self.getBlocks()
+        print(blocks)
+        for i in range(round):
+            print(i)
+            #print(blocks[0])
         return round
+
 if __name__ == '__main__':
     key_internal = [
             ['0xFF','0xF5', '0xF9', '0xF2'],
