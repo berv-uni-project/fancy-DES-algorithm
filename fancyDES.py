6f0e9ea0a311aad851404ba87ea52d855a387c7f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2018-03-10 09:10:47 +0700

    bring dynamic round

--- fancyDES.py
+++ fancyDES.py
@@ -1,4 +1,5 @@
 from collections import deque
+import random
 
 class FancyDES():
 
@@ -51,7 +52,18 @@ class FancyDES():
                 item.rotate(sum%4)
             output.append(list(item))
         return output
-    
+
+    def generate_chiper(self):
+        sum = 0
+        for i in self.key:
+            sum += ord(i)
+        print(sum)
+        random.seed(sum)
+        round = random.randint(1,25)
+        print(round)
+        if round < 7:
+            round = 7
+        return round
 if __name__ == '__main__':
     key_internal = [
             ['0xFF','0xF5', '0xF9', '0xF2'],
@@ -65,7 +77,8 @@ if __name__ == '__main__':
         ['0xFF','0xF5', '0xF4', '0x42'],
         ['0x6F','0x55', '0x24', '0x32'],
     ]
-    fancyDES = FancyDES('HELLO','HELLO')
+    fancyDES = FancyDES('HELLO','HELLO WORLD! HAHAHHA')
     #print(fancyDES.shift(message=block_right,key=key_internal)) 
     #print(fancyDES.transpose(block_right))
-    print(fancyDES.xor(block_right, key_internal))
+    #print(fancyDES.xor(block_right, key_internal))
+    print(fancyDES.generate_chiper())
