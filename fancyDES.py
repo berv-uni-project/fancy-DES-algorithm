1c2136bc092a77204ec7d9e13ea6ab3ccbc41c34
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2018-03-12 16:28:58 +0700

    Create generate internal keys method

--- fancyDES.py
+++ fancyDES.py
@@ -1,16 +1,58 @@
+#!/usr/bin/python3
 from collections import deque
+from pprint import pprint
+import sbox
+import hashlib
 import random
+import numpy as np
 
 class FancyDES():
+    internal_keys = []
 
     def __init__(self, path=None, message = None, key = None, fromFile = False):
         if (fromFile):
-            files = open(path, 'r')
-            self.message = files.read()   
+            files = open(path, 'rb')
+            self.message = files.read()
         else:
             self.message = message
-        
+
         self.key = key
+
+    def sub_sbox(self, block, box):
+        new_block = np.copy(block)
+        for i in range(4):
+            for j in range(4):
+                # cell = int(block[i][j], 16)
+                cell = block[i][j]
+                new_block[i][j] = sbox.sub(cell, box)
+        return new_block
+
+    def gen_internal_key(self, n_round):
+        h = hashlib.sha256()
+        h.update(self.key.encode('utf-8'))
+        key_hashed = list(h.digest())
+        # print (key_hashed, type(key_hashed), len(key_hashed))
+
+        # bagi ganjil-genap, XOR
+        odd = np.array(key_hashed[::2])
+        even = np.array(key_hashed[1::2])
+        # print(type(odd))
+        tmp = odd ^ even
+
+        # ubah ke block, tambahin ke internal_keys
+        block = np.array([tmp[x:x+4] for x in range(0, len(tmp), 4)])
+        self.internal_keys.append(block)
+        # print (block)
+
+        # generate other key
+        for i in range(n_round - 1):
+            new_block = self.sub_sbox(block, sbox.sbox)
+            new_block = block ^ new_block
+            self.internal_keys.append(new_block)
+            block = new_block
+
+        pprint(self.internal_keys)
+
 
     def message_to_block(self, message = ''):
         self.block_left = [
@@ -67,6 +109,7 @@ class FancyDES():
                 temp.append('0x{:02X}'.format(ord(message[pos])))
                 pos += 1
             out.append(temp)
+        # pprint(out)
         return out
 
     def messageToBlocks(self, n = 0, message = None):
@@ -88,6 +131,8 @@ class FancyDES():
         if sum_blocks % 2 == 1:
             sum_blocks += 1
             temp += '................'
+
+        print (temp, len(temp))
         blocks = self.messageToBlocks(sum_blocks, temp)
         return blocks
 
@@ -107,16 +152,18 @@ class FancyDES():
         return shift_result
 
     def generate_chiper(self):
+        # tentuin jumlah round via random
         sum = 0
         for i in self.key:
             sum += ord(i)
         random.seed(sum)
-        round = random.randint(1,25)
-        if round < 7:
-            round = 7
+        round = random.randint(7,25)
+
+        # ubah message ke block
         self.position = 0
         blocks = self.getBlocks()
         number_of_blocks = len(blocks)
+
         # TODO: Generate key internal
         key_internal = [
             ['0xFF','0xF5', '0xF9', '0xF2'],
@@ -124,6 +171,7 @@ class FancyDES():
             ['0xFF','0xF5', '0x64', '0x42'],
             ['0x6F','0x55', '0x53', '0x32'],
         ]
+
         out_blocks = []
         for iter_num in range(number_of_blocks // 2):
             block_left = blocks[iter_num]
@@ -153,10 +201,22 @@ class FancyDES():
 
 if __name__ == '__main__':
     fancyDES = FancyDES(path='samples/text.txt',key = 'HELLO WORLD! HAHAHHA', fromFile=True)
-    chiper = fancyDES.generate_chiper()
-    print('Encrypted:')
-    print(chiper)
-    fancyDES1 = FancyDES(message=chiper, key = 'HELLO WORLD! HAHAHHA', fromFile=False)
-    plainteks = fancyDES1.generate_chiper()
-    print('Decrypted:')
-    print(plainteks)
+    # chiper = fancyDES.generate_chiper()
+    # print('Encrypted:')
+    # print(chiper)
+    # fancyDES1 = FancyDES(message=chiper, key = 'HELLO WORLD! HAHAHHA', fromFile=False)
+    # plainteks = fancyDES1.generate_chiper()
+    # print('Decrypted:')
+    # print(plainteks)
+
+    fancyDES.gen_internal_key(7)
+    # block = [
+    #     ['0xFF','0xF5', '0xF9', '0xF2'],
+    #     ['0x5F','0x35', '0x25', '0x12'],
+    #     ['0xFF','0xF5', '0x64', '0x42'],
+    #     ['0x6F','0x55', '0x53', '0x32'],
+    # ]
+    # block = fancyDES.sub_sbox(block)
+    # for row in block:
+    #     for el in row:
+    #         print('0x{:02X}'.format(el))
